7985fbf5f01802644792585498a9a7f416a521ae
Laion Corcino <laioncorcino@gmail.com>
2024-05-28 15:43:17 -0300

    [chore] adding lib rabbit

--- src/main/java/com/corcino/users/UsersApplication.java
+++ src/main/java/com/corcino/users/UsersApplication.java
@@ -1,9 +1,11 @@
 package com.corcino.users;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class UsersApplication {
 
 	public static void main(String[] args) {
